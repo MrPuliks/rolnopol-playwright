ab004b3c1c544d2a590a12451ce405f0581bd47c
MrPuliks <puliks16@gmail.com>
2026-05-22 10:09:29 +0200

    JIRA-1: Typescript and Playwright basic setup

---
+++ playwright.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env['CI'],
+  retries: 1,
+  workers: 3,
+  reporter: [["html"], ["list"]],
+  use: {
+    headless: false,
+    trace: 'retain-on-failure',
+    video: "on",
+    screenshot: "on",
+    actionTimeout: 10 * 1000,
+    navigationTimeout: 30 * 1000
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    }
+  ],
+
+});
