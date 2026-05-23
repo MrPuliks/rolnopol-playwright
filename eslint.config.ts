a39c653a21a992e00d7c136e103a7240d1c6c160
MrPuliks <puliks16@gmail.com>
2026-05-23 13:55:37 +0200

    JIRA-2: ESLint and Prettier setup

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,24 @@
 import js from "@eslint/js";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  js.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/await-thenable": "error",
+    },
+  },
 ]);
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,35 +1,34 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   fullyParallel: true,
-  forbidOnly: !!process.env['CI'],
+  forbidOnly: !!process.env["CI"],
   retries: 1,
   workers: 3,
   reporter: [["html"], ["list"]],
   use: {
     headless: true,
-    trace: 'retain-on-failure',
+    trace: "retain-on-failure",
     video: "on",
     screenshot: "on",
     actionTimeout: 10 * 1000,
-    navigationTimeout: 30 * 1000
+    navigationTimeout: 30 * 1000,
   },
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    }
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
-
 });
