2a8976f21f1f186c5c267b0a138703d6b3f1eddb
MrPuliks <puliks16@gmail.com>
2026-05-24 14:01:15 +0200

    JIRA-6: Git hooks and prettier

--- src/pages/NavigationBar.ts
+++ src/pages/NavigationBar.ts
@@ -1,5 +1,13 @@
+import { type Page, type Locator } from "@playwright/test";
 
-export class NavigationBar
-{
-    constructor(private readonly page: Page) { } 
-} 
+export class NavigationBar {
+  private homeButton: Locator;
+
+  constructor(private readonly page: Page) {
+    this.homeButton = this.page.getByTestId("nav-home");
+  }
+
+  async clickHome() {
+    await this.homeButton.click();
+  }
+}
